e03f0169fac4212f57dac9bbd29ba1ba7aed1e37
Raman Sahi <sahiraman7@gmail.com>
2024-08-08 20:33:47 +0530

    first commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // connect to database
 mongoose.connect(
-  "mongodb+srv://sahiraman7:Y8oBMVfbG6YlPUzj@cluster0.dncnotl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://sahiraman7:Y8oBMVfbG6YlPUzj@cluster0.dncnotl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0y"
 );
 
 // check if connection is successful
