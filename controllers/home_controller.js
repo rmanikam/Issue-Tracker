c5b5560cf46784ef1152399b7b559de0be17993d
Raman Sahi <sahiraman7@gmail.com>
2024-08-08 20:48:57 +0530

    changes

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,7 +5,6 @@ module.exports.home = async function (req, res) {
   try {
     // Fetch all the projects from database
     let projects = await Project.find({});
-
     return res.render("home", {
       title: "Issue Tracker | Home",
       projects: projects,
